90f6925fc5e52be52000ecbe46dfcc5ac382098e
Ivan Adzic <121823205+ivanadzic@users.noreply.github.com>
2023-02-11 16:41:55 +0100

    Add files via upload

---
+++ Exercise3.java
@@ -0,0 +1,31 @@
+package GitHub;
+
+import java.util.Scanner;
+
+public class Exercise3 {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the year of the start of work:");
+    int startOfWork = sc.nextInt();
+
+    System.out.println("Enter the current year:");
+    int currentYear = sc.nextInt();
+
+    int lengthOfService = currentYear - startOfWork;
+
+    if (lengthOfService >= 0 && lengthOfService < 40) {
+      if (lengthOfService <= 4) {
+        System.out.println("You are a Junior Programmer");
+      } else if (lengthOfService <= 8) {
+        System.out.println("You are a Senior Programmer.");
+      } else {
+        System.out.println("You are a Company director.");
+      }
+    } else {
+      System.out.println("You are retired.");
+    }
+
+  }
+}
