9333dad27641b938256bc5b0662a2428b166fd7e
Ivan Adzic <121823205+ivanadzic@users.noreply.github.com>
2023-02-11 15:52:16 +0100

    Update FruitBasket.java

--- FruitBasket.java
+++ FruitBasket.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class FruitBasket {
   public static void main(String[] args) {
-    // Napraviti program gde cete uneti informacije o nekom vocu
-    // tako sto cete kreirati String za naziv nekog voca i double vrednost za njegovu cenu.
-    // Uneti 4 naziva i 4 cene za odredjeno voce,
-    // i u svakom redu posebno stampati naziv vocke i njenu cenu za 30% snizenu.
 
     Scanner sc = new Scanner(System.in);
 
